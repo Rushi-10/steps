39353baed940d7a13cb53045557f580e8ae5e647
rushi10 <rushimadhure10@gmail.com>
2024-06-30 16:38:27 +0530

    Reusable button with children prop

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   function handleNext(){
     if(step<3) 
     setStep((step)=>step+1);
-    setStep((step)=>step+1);
+    
   }
   function handleOpen(){
     setIsOpen(!isOpen);
@@ -32,12 +32,21 @@ function App() {
     </div>
     <p className="message">Step {step}: {messages[step-1]}</p>
     <div className="buttons">
-    <button style={{ backgroundColor:"#7950f2", color:"#fff"}} onClick={()=>handlePrevious()}>Previous</button>
-    <button style={{ backgroundColor:"#7950f2", color:"#fff"}} onClick={()=>handleNext()}>Next</button>
+    <Button bgColor="#7950f2" textColor="#fff" onClick={()=>handlePrevious()}>
+    <span>👈</span>Previous</Button>
+    <Button bgColor="#7950f2" textColor="#fff" onClick={()=>handleNext()}>
+    Next<span>👉</span></Button>
     </div>
     </div>
     }
     </div>
+  );
+}
+function Button({textColor,bgColor,onClick,children}){
+  return(
+<button style={{backgroundColor:bgColor,color:textColor}} onClick={onClick}>
+{children}
+</button>
   );
 }
 
